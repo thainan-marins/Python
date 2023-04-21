bf7e13e1fe13a0c95ff0f20a58e53bf80cfc3906
thainan-marins <98652034+thainan-marins@users.noreply.github.com>
2023-04-21 11:20:58 -0300

    news

--- exercicios/ex 001 - Deixando Tudo Pronto .py
+++ exercicios/ex 001 - Deixando Tudo Pronto .py
@@ -6,8 +6,6 @@ print('\033[1;34m-=-' * 7)
 print('\033[32m Olá, Mundo!\033[m')
 # aqui printamos o emoji usando: da biblioteca emoji pegue o método emojize(que serve para trancrever as palavras
 # em emojis.
-# o parâmetro 'use_aliases=True' serve para ampliar a quantidade dos emoticons além dos oficiais.
-print(emoji.emojize(':wave:' * 5, use_aliases=True))
+# o parâmetro 'language' serve para definir a língua do emoji, sem esse parâmetro ele não funcionará
+print(emoji.emojize(':wave:' * 5, language='alias'))
 # finalizamos retpetindo uma string com o código de cor
-print('\033[1;34m-=-' * 7)
-
